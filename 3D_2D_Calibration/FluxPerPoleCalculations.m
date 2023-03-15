dcc176f099aa89d90145ab4bfc87b8c35e471c91
Samet <samet.yakut@metu.edu.tr>
2023-03-15 11:36:04 +0300

    Flux Per Pole Calculation
    Calculations of flux per pole for 3d and 2d models are added

--- 3D_2D_Calibration/FluxPerPoleCalculations.m
+++ 3D_2D_Calibration/FluxPerPoleCalculations.m
@@ -14,30 +14,40 @@
 
 % This is the first version: published in 15.03.2023
 %% Variables
-data3d = [];
+clear all
+clc
+close all
+
+data3d = xlsread("fpp3d.csv");
 data2d = xlsread("fpp2d.csv");
 If = unique(data2d(:,1));
 
 fpp2d = zeros(15,1); % flux per pole array of 2D results
 fpp3d = zeros(15,1); % flux per pole array of 3D results
 %% Flux Per Pole Calculations
-sum = 0; % summation for surfaces of 3D model, i.e., 160 surfaces at each pole
-j = 1; % just an index
-for i = 1:length(data2d) % finding related fpp from 2D results data
-    fpp2d(i) = max(data2d(i));
+NumSurfaces = 1280; % number of surfaces in quarter symmetry
+poles = 8; % number of poles in quarter symmetry
+PoleSurface = NumSurfaces/poles; % number of surfaces in front of a pole
+data3d = data3d(1:end,2:PoleSurface+1);
+data2d = reshape(data2d,102,[]);
+data2d = data2d(1:end,31:end);
+
+
+for i = 1:length(If) % finding related fpp from 2D results data
+    fpp2d(i) = max(data2d(1:end,i));
 end
 
-for i = 1:160 % integration of flux for 3D results
-    sum = sum + data3d(i);
-    if i == 160
-        fpp3d(j) = sum;
-        j = j+1;
-    end
+
+for j = 1:length(If)
+    fpp3d(j) = sum(data3d(j,1:end));
 end
+
+fpp3d = 2*fpp3d; % due to half symmetry in the axial direction
 
 %% Plotting
-scatter(If,fpp3d);
+scatter(If,fpp3d,'LineWidth',2);
 hold on
-scatter(If,fpp2d);
+scatter(If,fpp2d,'LineWidth',2);
+legend('3D','2D')
 
 %% Slot Leakage Flux Calculations
