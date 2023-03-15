d8a3530998ed8393616c6a399af425d8d81a10bb
Samet <samet.yakut@metu.edu.tr>
2023-03-15 10:46:46 +0300

    3D Solutions Added

--- 3D_2D_Calibration/FluxPerPoleCalculations.m
+++ 3D_2D_Calibration/FluxPerPoleCalculations.m
@@ -1,3 +1,18 @@
+% This basic script aims to calibrate the ANSYS Maxwell 3D and 2D in terms
+% of axial length of a large scale salient pole synchronous machine. The
+% methodology is to calculate the flux per poles in both Maxwell 3D and 2D,
+% so that see the how axial length differs. The analytically calculated
+% effective axial length from Carter's equations is 913.46 mm, where the
+% actual axial length is 925 mm. The input model depth of 2D model is
+% analytically calculated effective axial length. Note that 2g (2*airgap)
+% term is ignored during effective length calculation because the stator
+% core ends have 3 steps to reduce fringing flux. In 3D model, those
+% fringings are also included. 
+
+% Further, leakage fluxes of the slots will be calculated to check the
+% Roebel transposition validities.
+
+% This is the first version: published in 15.03.2023
 %% Variables
 data3d = [];
 data2d = xlsread("fpp2d.csv");
